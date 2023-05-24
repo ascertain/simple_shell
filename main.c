9ad8a701cbaa6e46787c7dbd98b6824c81dc1cdf
ascertain <kas.naya@gmail.com>
2023-05-24 01:39:30 -0700

    some update

--- main.c
+++ main.c
@@ -1,34 +1,73 @@
-#include "shell.h"
+#include "holberton.h"
 
-int main(void)
+/**
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
+ */
+void free_data(data_shell *datash)
 {
-    char buffer[BUFFER_SIZE];
-	char *args[BUFFER_SIZE];
-    int n_read;
-	char *token;
-	int i;
+	unsigned int i;
 
-    while (1)
-    {
-        _displayPrompt();
-        n_read = _read(buffer, BUFFER_SIZE);
-        if (n_read == 0)
-        {
-            printf("\n");
-            break;
-        }
-        buffer[n_read - 1] = '\0';
-		token = strtok(buffer, " ");
-		i = 0;
-		while (token != NULL)
-		{
-			args[i] = token;
-			token = strtok(NULL, " ");
-			i++;
-		}
-		args[i] = NULL;
-        _execute(args[0], args);
-    }
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
 
-    return 0;
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
